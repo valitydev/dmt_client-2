bf58e0be987b5f9c2d83ef6ac7a65931ab600a14
Yaroslav Rogov <rogovyaroslav@gmail.com>
2021-06-17 17:37:37 +0300

    feat: Add implicit head API, easier ref passing

--- src/dmt_client.erl
+++ src/dmt_client.erl
@@ -9,14 +9,19 @@
 %% API
 -export([get_snapshot/1]).
 -export([get_snapshot/2]).
+-export([get/1]).
 -export([get/2]).
 -export([get/3]).
+-export([get_versioned/1]).
 -export([get_versioned/2]).
 -export([get_versioned/3]).
+-export([get_by_type/1]).
 -export([get_by_type/2]).
 -export([get_by_type/3]).
+-export([filter/1]).
 -export([filter/2]).
 -export([filter/3]).
+-export([fold/2]).
 -export([fold/3]).
 -export([fold/4]).
 -export([commit/2]).
@@ -53,6 +58,7 @@
 
 -type ref() :: dmsl_domain_config_thrift:'Reference'().
 -type version() :: dmsl_domain_config_thrift:'Version'().
+-type ref_arg() :: ref() | version() | head.
 -type limit() :: dmsl_domain_config_thrift:'Limit'().
 -type snapshot() :: dmsl_domain_config_thrift:'Snapshot'().
 -type commit() :: dmsl_domain_config_thrift:'Commit'().
@@ -68,11 +74,11 @@
 
 %%% API
 
--spec get_snapshot(ref()) -> snapshot() | no_return().
+-spec get_snapshot(ref_arg()) -> snapshot() | no_return().
 get_snapshot(Reference) ->
     get_snapshot(Reference, undefined).
 
--spec get_snapshot(ref(), transport_opts()) -> snapshot() | no_return().
+-spec get_snapshot(ref_arg(), transport_opts()) -> snapshot() | no_return().
 get_snapshot(Reference, Opts) ->
     Version = ref_to_version(Reference),
     case dmt_client_cache:get_snapshot(Version, Opts) of
@@ -82,38 +88,54 @@ get_snapshot(Reference, Opts) ->
             erlang:error(Error)
     end.
 
--spec get(ref(), object_ref()) -> domain_object() | no_return().
+-spec get(object_ref()) -> domain_object() | no_return().
+get(ObjectReference) ->
+    get(head, ObjectReference).
+
+-spec get(ref_arg(), object_ref()) -> domain_object() | no_return().
 get(Reference, ObjectReference) ->
     get(Reference, ObjectReference, undefined).
 
--spec get(ref(), object_ref(), transport_opts()) -> domain_object() | no_return().
+-spec get(ref_arg(), object_ref(), transport_opts()) -> domain_object() | no_return().
 get(Reference, ObjectReference, Opts) ->
     Version = ref_to_version(Reference),
-    unwrap(dmt_client_cache:get_object(Version, ObjectReference, Opts)).
+    unwrap(dmt_client_cache:get(Version, ObjectReference, Opts)).
 
--spec get_versioned(ref(), object_ref()) -> versioned_object() | no_return().
+-spec get_versioned(object_ref()) -> versioned_object() | no_return().
+get_versioned(ObjectReference) ->
+    get_versioned(head, ObjectReference).
+
+-spec get_versioned(ref_arg(), object_ref()) -> versioned_object() | no_return().
 get_versioned(Reference, ObjectReference) ->
     get_versioned(Reference, ObjectReference, undefined).
 
--spec get_versioned(ref(), object_ref(), transport_opts()) -> versioned_object() | no_return().
+-spec get_versioned(ref_arg(), object_ref(), transport_opts()) -> versioned_object() | no_return().
 get_versioned(Reference, ObjectReference, Opts) ->
     Version = ref_to_version(Reference),
     #'VersionedObject'{version = Version, object = get(Reference, ObjectReference, Opts)}.
 
--spec get_by_type(ref(), object_type()) -> [domain_object()] | no_return().
+-spec get_by_type(object_type()) -> [domain_object()] | no_return().
+get_by_type(ObjectType) ->
+    get_by_type(head, ObjectType).
+
+-spec get_by_type(ref_arg(), object_type()) -> [domain_object()] | no_return().
 get_by_type(Reference, ObjectType) ->
     get_by_type(Reference, ObjectType, undefined).
 
--spec get_by_type(ref(), object_type(), transport_opts()) -> [domain_object()] | no_return().
+-spec get_by_type(ref_arg(), object_type(), transport_opts()) -> [domain_object()] | no_return().
 get_by_type(Reference, ObjectType, Opts) ->
     Version = ref_to_version(Reference),
     unwrap(dmt_client_cache:get_by_type(Version, ObjectType, Opts)).
 
--spec filter(ref(), object_filter()) -> [{object_type(), domain_object()}] | no_return().
+-spec filter(object_filter()) -> [{object_type(), domain_object()}] | no_return().
+filter(Filter) ->
+    filter(head, Filter).
+
+-spec filter(ref_arg(), object_filter()) -> [{object_type(), domain_object()}] | no_return().
 filter(Reference, Filter) ->
     filter(Reference, Filter, undefined).
 
--spec filter(ref(), object_filter(), transport_opts()) -> [{object_type(), domain_object()}] | no_return().
+-spec filter(ref_arg(), object_filter(), transport_opts()) -> [{object_type(), domain_object()}] | no_return().
 filter(Reference, Filter, Opts) ->
     Folder = fun(Type, Object, Acc) ->
         case Filter(Type, Object) of
@@ -123,11 +145,15 @@ filter(Reference, Filter, Opts) ->
     end,
     fold(Reference, Folder, [], Opts).
 
--spec fold(ref(), object_folder(Acc), Acc) -> Acc | no_return().
+-spec fold(object_folder(Acc), Acc) -> Acc | no_return().
+fold(Folder, Acc) ->
+    fold(head, Folder, Acc).
+
+-spec fold(ref_arg(), object_folder(Acc), Acc) -> Acc | no_return().
 fold(Reference, Folder, Acc) ->
     fold(Reference, Folder, Acc, undefined).
 
--spec fold(ref(), object_folder(Acc), Acc, transport_opts()) -> Acc | no_return().
+-spec fold(ref_arg(), object_folder(Acc), Acc, transport_opts()) -> Acc | no_return().
 fold(Reference, Folder, Acc, Opts) ->
     Version = ref_to_version(Reference),
     unwrap(dmt_client_cache:fold(Version, Folder, Acc, Opts)).
@@ -189,6 +215,10 @@ unwrap({error, {woody_error, _} = Error}) -> erlang:error(Error);
 unwrap({error, _}) -> erlang:throw(#'ObjectNotFound'{}).
 
 -spec ref_to_version(ref()) -> version().
+ref_to_version(Version) when is_integer(Version) ->
+    Version;
+ref_to_version(head) ->
+    dmt_client_cache:get_last_version();
 ref_to_version({version, Version}) ->
     Version;
 ref_to_version({head, #'Head'{}}) ->
