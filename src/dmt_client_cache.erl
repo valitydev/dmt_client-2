c6f09fd6404950369648d1da5a2f2ff4bfc41517
Yaroslav Rogov <rogovyaroslav@gmail.com>
2021-06-23 11:16:40 +0300

    fix: Remove stale unlock cast

--- src/dmt_client_cache.erl
+++ src/dmt_client_cache.erl
@@ -154,7 +154,6 @@ get_last_version() ->
 update() ->
     case call(update) of
         {fetched, Version} ->
-            cast({unlock, Version, self()}),
             {ok, Version};
         AsIs ->
             AsIs
